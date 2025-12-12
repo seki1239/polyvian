705831ec41bb17ed4d3561752ac005812ade6ae5
seki1239 <kennelectric@gmail.com>
2025-12-12 22:45:08 +0900

    fix: 類似語ポップアップの修正とUIレイアウト修正

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g8dvvsk7qv4"
+    "revision": "0.q3kn207etjs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
